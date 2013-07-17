cc1ac6bccdc4ecd061c94360aab482e98af0d0c7
Tiago Lammers <lammers@outlook.com>
2013-07-17 13:45:08 -0300

    Reading index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require("fs");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  
+  var buf = fs.readFile("index.html");
+  response.send( buf.toString('utf8', 0, buf.length) );
+
 });
 
 var port = process.env.PORT || 5000;
